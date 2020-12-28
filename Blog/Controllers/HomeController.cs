5387a884a3b033f4938111c69d8637a95f485e50
Максим Стрельцов <green1971weekend1@gmail.com>
2020-12-28 16:19:00 +0300

    feat: post functionality added (edit, remove)

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -18,31 +18,51 @@ namespace Blog.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var posts = _repository.GetAll();
+            return View(posts);
         }
 
-        public IActionResult Post()
+        public IActionResult Post(int id)
         {
-            return View();
+            var post = _repository.Get(id);
+
+            return View(post);
         }
 
         [HttpGet]
-        public IActionResult Edit()
+        public async Task<IActionResult> Remove(int id)
         {
-            return View(new Post());
+            _repository.Remove(_repository.Get(id));
+            await _repository.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return View(new Post());
+            }
+
+            var post = _repository.Get((int) id);
+            return View(post);
         }
 
         [HttpPost]
         public async Task<IActionResult> Edit(Post post)
         {
-            _repository.Add(post);
+            if(post.Id > 0)
+                _repository.Update(post);
+            else
+                _repository.Add(post);
+
 
             if(await _repository.SaveChangesAsync())
-            {
                 return RedirectToAction("Index");
-            }
-
-            return View(post);
+            else
+                return View(post);
         }
     }
 }
